8f9b5ff8529e9ac17a2b9f595d9a4f1f5dbb9039
swaggerloaf <swaggerloaf@gmail.com>
2019-01-28 20:25:30 -0500

    Updates

--- src/app.module.js
+++ src/app.module.js
@@ -1,18 +1,15 @@
 // app.js
-import angular from 'angular';
-import  button from '../node_modules/angular-ui-bootstrap/src/buttons/index';
-import MainController from './main.controller';
-import CounterModule from './counter/index.js';
+import angular from "angular";
+import MainController from "./main.controller";
+import CounterModule from "./counter/index.js";
 
 // our root module
 const AppModule = angular
-  .module('parkApp', [ button,
-    CounterModule
-  ])
-  .controller('MainController', MainController)
-  .run( () =>  // instance-injector
-    console.log('Angular version ' + angular.version.full)
-  )
-  .name;
+  .module("parkApp", [CounterModule])
+  .controller("MainController", MainController)
+  .run(() =>
+    // instance-injector
+    console.log("Angular version " + angular.version.full)
+  ).name;
 
 export default AppModule;
