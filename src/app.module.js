5f025b29ac93821656e53f1dc1f92f03d75f12d6
swaggerloaf <swaggerloaf@gmail.com>
2019-02-26 19:15:37 -0500

    Updates

--- src/app.module.js
+++ src/app.module.js
@@ -1,15 +1,12 @@
 // app.js
-import angular from "angular";
-import MainController from "./main.controller";
-import CounterModule from "./counter/index.js";
+import angular from 'angular';
+import MainController from './main.controller';
+import CounterModule from './counter/index.js';
 
 // our root module
 const AppModule = angular
-  .module("parkApp", [CounterModule])
-  .controller("MainController", MainController)
-  .run(() =>
-    // instance-injector
-    console.log("Angular version " + angular.version.full)
-  ).name;
+  .module('parkApp', [CounterModule])
+  .controller('MainController', MainController)
+  .run(() => console.log('Angular version ' + angular.version.full)).name;
 
 export default AppModule;
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,16 @@ module.exports = {
     {
       test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
       loader: 'file-loader'
-    }]
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader', // creates style nodes from JS strings
+        'css-loader', // translates CSS into CommonJS
+        'sass-loader' // compiles Sass to CSS, using Node Sass by default
+      ]
+    }
+  ],
   },
   context: __dirname,
   plugins: [
